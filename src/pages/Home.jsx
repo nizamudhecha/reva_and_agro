91536ef0b5ac44d1d7334084ac9a1eefd18ab412
nizamudhecha <139213653+nizamudhecha@users.noreply.github.com>
2025-07-13 00:18:58 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Hero from "../components/hero";
+import Hero from "../components/Hero";
 
 import OurPolicy from "../components/OurPolicy";
 import NewsLetterBox from "../components/NewsLetterBox";
