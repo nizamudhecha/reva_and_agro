aa65f13379f5a1a70fd92973959f8a46553ba83c
luciifer <nizamudhecha@users.noreply.github.com>
2025-07-12 23:11:48 +0530

    fix: add vite to devDependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.css";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import About from "./pages/about";
 import Collection from "./pages/collection";
 import Contact from "./pages/Contact";
